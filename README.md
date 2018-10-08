7462050bb0457d17114da1b004ba3fc4e3ad3621
nguyen van dien <dienvx1997bn@gmail.com>
2018-10-08 23:41:36 +0700

    add transaction

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Dchain
-Link tutorial: https://medium.com/programmers-blockchain/create-simple-blockchain-java-tutorial-from-scratch-6eeed3cb03fa
+Link tutorial ver1: https://medium.com/programmers-blockchain/create-simple-blockchain-java-tutorial-from-scratch-6eeed3cb03fa
+Link tutorial ver2: https://medium.com/programmers-blockchain/creating-your-first-blockchain-with-java-part-2-transactions-2cdac335e0ce
--- src/com/dien/Block.java
+++ src/com/dien/Block.java
@@ -1,23 +1,22 @@
 package com.dien;
 
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data; //our data will be a simple message.
-    private long timeStamp; //as number of milliseconds since 1/1/1970.
-    private int nonce;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+    public long timeStamp; //as number of milliseconds since 1/1/1970.
+    public int nonce;
 
     //Block Constructor.
-    public Block(String data,String previousHash ) {
-        this.data = data;
+    public Block(String previousHash ) {
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
+
         this.hash = calculateHash(); //Making sure we do this after we set the other values.
     }
 
@@ -27,29 +26,37 @@ public class Block {
                 previousHash +
                         Long.toString(timeStamp) +
                         Integer.toString(nonce) +
-                        data
+                        merkleRoot
         );
         return calculatedhash;
     }
-    /*
-    Tạo block mới sao cho khi hash block này thì được mã hash có độ dài số 0 ở đầu mã tương ứng với số difficulty
-     */
+
+    //Increases nonce value until hash target is reached.
     public void mineBlock(int difficulty) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        //String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0"
         String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+
         while(!hash.substring( 0, difficulty).equals(target)) {
-            nonce ++;                   //Tăng dần giá trị noce để hash ra giá trị mới. Về bản chất là tìm ra số nunce mỗi lần hast
+            nonce ++;
             hash = calculateHash();
         }
-        System.out.print("Block Mined!!! : " + hash);
-        //hiển thị giá trị noce
-        System.out.print("\tnonce: " + nonce);
-        //hiển thị thời gian thực hiện đào
-        Date currentDate = new Date(timeStamp);
-        DateFormat df = new SimpleDateFormat("dd:MM:yy:HH:mm:ss");
-        System.out.println("\tTime: " + df.format(currentDate));
-
-
-
-
+        System.out.println("Block Mined!!! : " + hash);
     }
+
+    //Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((previousHash != "0")) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
+    }
+
 }
