fe224d94fbf4a59878c0b169f49abc38f6081b9f
nguyen van dien <dienvx1997bn@gmail.com>
2018-10-08 22:45:32 +0700

    add mineBlock function

--- src/com/dien/Block.java
+++ src/com/dien/Block.java
@@ -1,5 +1,8 @@
 package com.dien;
 
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Block {
@@ -15,7 +18,6 @@ public class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
-
         this.hash = calculateHash(); //Making sure we do this after we set the other values.
     }
 
@@ -29,13 +31,25 @@ public class Block {
         );
         return calculatedhash;
     }
-
+    /*
+    Tạo block mới sao cho khi hash block này thì được mã hash có độ dài số 0 ở đầu mã tương ứng với số difficulty
+     */
     public void mineBlock(int difficulty) {
         String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
         while(!hash.substring( 0, difficulty).equals(target)) {
-            nonce ++;
+            nonce ++;                   //Tăng dần giá trị noce để hash ra giá trị mới. Về bản chất là tìm ra số nunce mỗi lần hast
             hash = calculateHash();
         }
-        System.out.println("Block Mined!!! : " + hash);
+        System.out.print("Block Mined!!! : " + hash);
+        //hiển thị giá trị noce
+        System.out.print("\tnonce: " + nonce);
+        //hiển thị thời gian thực hiện đào
+        Date currentDate = new Date(timeStamp);
+        DateFormat df = new SimpleDateFormat("dd:MM:yy:HH:mm:ss");
+        System.out.println("\tTime: " + df.format(currentDate));
+
+
+
+
     }
 }
